4f78eebaaa75bbc87d520d1586efafaa675dcae3
Emanuelevit <emanuele.romualdo@escola.pr.gov.br>
2026-03-30 09:25:10 -0300

    iniciando atividade de operadores

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-// run `node index.js` in the terminal
+{
+const num1 = 79;
+const num2 = 16;
+soma = num1 + num2;
+console.log ("Resultado foi: " , soma);
+}
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+{
+let1 = 50;
+let2 = 25;
+subtração = let1 - let2;
+console.log ("o resultado será:  ", subtração);
+}
+
+{
+const1 = 34;
+const3 = 78;
+multiplicação = const1 * const3;
+}
+
+{
+const num3 = 100;
+const num7 = 4;
+divisão = num3 / num7;
+console.log ("O valor do quociente: ", divisão);
+}
